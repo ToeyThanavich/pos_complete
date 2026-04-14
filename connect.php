0c064c83a0e5c876d0ee6fd1b7f6c3fc69c5b973
ToeyThanavich <14thanavich1it@gmail.com>
2026-04-14 20:33:01 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,17 @@
 <?php
-  date_default_timezone_set('Asia/Bangkok');
+date_default_timezone_set('Asia/Bangkok');
 
-  $host   = "localhost";
-  $user   = "root";
-  $pass   = "";
-  $dbname = "cafe_pos";
+$host   = "localhost";
+$user   = "if0_41661118";
+$pass   = "0VQznpHBk9Ts1yA";
+$dbname = "if0_41661118_cafe_pos";
 
-  $conn = mysqli_connect($host, $user, $pass, $dbname)
-      or die('ไม่สามารถเชื่อมต่อกับ database ได้');
-  $conn->query("SET NAMES UTF8");
-  $conn->query("SET time_zone = '+07:00'");
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+    die("DB connection failed: " . mysqli_connect_error());
+}
+
+$conn->query("SET NAMES utf8mb4");
+$conn->query("SET time_zone = '+07:00'");
 ?>
